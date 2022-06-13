3af6e92f9fb46faeb749b08842e4955166088081
Michael Wu <wuxiaohua1011@gmail.com>
2022-06-12 21:13:51 -0700

    berkeley version is now the main version

--- launch/roar_carla.launch.py
+++ launch/roar_carla.launch.py
@@ -10,8 +10,8 @@ def generate_launch_description():
     base_path = os.path.realpath(
         get_package_share_directory("roar_carla_ros2")
     )  # also tried without realpath
-    rviz_path = base_path + "/config/rviz_gary.rviz"
-    vehicle_config_path = base_path + "/config/Kart_Gary.json"
+    rviz_path = base_path + "/config/rviz_berkeley.rviz"
+    vehicle_config_path = base_path + "/config/berkeley_gokart.json"
 
     ld = launch.LaunchDescription(
         [
